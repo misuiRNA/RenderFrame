103fbacb93318aae3cfae8952a223c3ad16798f4
nirui02 <nirui02@baidu.com>
2024-11-12 17:00:17 +0800

    issues: optimize LineUtils::LineToLane

--- code/include/render/RenderEngine.h
+++ code/include/render/RenderEngine.h
@@ -60,6 +60,7 @@ struct RenderShape {
         Color color;
     };
 
+    RenderShape() {}
     RenderShape(const std::vector<Vertex>& vertices, const std::vector<unsigned int>& indices = {}) : vertices(vertices), indices(indices) { }
 
     std::vector<Vertex> vertices;
--- code/src/object/Trace.cpp
+++ code/src/object/Trace.cpp
@@ -5,7 +5,7 @@ Trace::Trace()
 : _shader({1.0f, 1.0f, 1.0f}) {
     // _shader.setDrawMode(RenderDataMode::LINE_STRIP);
     // _shader.setPosition({-2.0f, 3.0f, 5.0f});
-    _shader.setColor(Color(0.0f, 1.0f, 0.0f));
+    _shader.setColor(Color(1.0f, 1.0f, 0.0f));
     _shader.getAttituedeCtrl().setFront({0.0f, 0.0f, 1.0f}).setUp({0.0f, 1.0f, 0.0f});
 }
 
@@ -15,6 +15,12 @@ void Trace::show() {
 
 void Trace::append(const Position& pos) {
     _MidLinePosits.emplace_back(pos);
+    RenderShape laneShape = LineUtils::LineToLane(_MidLinePosits, 0.2f);
+    _shader.setVertexData(laneShape);
+}
+
+void Trace::append(const std::vector<Position>& points) {
+    _MidLinePosits.insert(_MidLinePosits.end(), points.begin(), points.end());
     RenderShape laneShape = LineUtils::LineToLane(_MidLinePosits, 0.3f);
     _shader.setVertexData(laneShape);
 }
